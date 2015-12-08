bf977b1f28325eb908affd0485b2ad9bd10d41df
Amir Hoseinian <amir.anyware@gmail.com>
2015-12-08 18:26:31 +0330

    style changes

--- models/product.js
+++ models/product.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose')
-	, Schema = mongoose.Schema;
+	, Schema = mongoose.Schema
+	, uniqueValidator = require('mongoose-unique-validator');
 
 var ProductSchema = new Schema({
-  code: {type: String, required: true},
+  code: {type: String, required: true, unique: true},
   _model: {type: Schema.Types.ObjectId, ref: 'Model'}
 }, { timestamps: { createdAt: 'created_at' }, strict: false});
+
+ProductSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Product', ProductSchema);
